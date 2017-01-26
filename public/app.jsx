832f2a5460a44476531609965e5d54b46a34ee1f
Moufid Larbi <moufid4@gmail.com>
2017-01-26 10:22:07 -0500

    added props

--- public/app.jsx
+++ public/app.jsx
@@ -1,14 +1,17 @@
 var Greeter = React.createClass({
 	render: function() {
+		var name = this.props.name;
+
 		return (
 			<div>
-				<h1>Hello Moufid!</h1>			
+				<h1>Hello {name}!</h1>
+				<p>This is from React component.</p>			
 			</div>
 		);
 	}
 });
 
 ReactDOM.render(
-	<Greeter/>,
+	<Greeter name="Moufid"/>,
 	document.getElementById('app')
 );
