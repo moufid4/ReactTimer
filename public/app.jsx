f66158ccb87f307a262045e1fd2834b1cf3225c8
Moufid Larbi <moufid4@gmail.com>
2017-01-26 10:24:26 -0500

    added defaultProps

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,9 @@
 var Greeter = React.createClass({
+	getDefaultProps: function() {
+		return {
+			name: 'React'
+		};
+	},
 	render: function() {
 		var name = this.props.name;
 
@@ -12,6 +17,6 @@ var Greeter = React.createClass({
 });
 
 ReactDOM.render(
-	<Greeter name="Moufid"/>,
+	<Greeter/>,
 	document.getElementById('app')
 );
