4fac19f95526fd236b5467a3a30c4ea8930ad794
Moufid Larbi <moufid4@gmail.com>
2017-01-26 10:10:38 -0500

    created first react component

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,14 @@
+var Greeter = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<h1>Hello Moufid!</h1>			
+			</div>
+		);
+	}
+});
+
 ReactDOM.render(
-	<h1>Hello Moufid!</h1>,
+	<Greeter/>,
 	document.getElementById('app')
 );
