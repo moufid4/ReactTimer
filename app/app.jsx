3c1a445249f737e85a5a4d5411d88990c9646b8d
Moufid Larbi <moufid4@gmail.com>
2017-01-26 19:41:02 -0500

    added experimental javascript feature

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objOne = {
+	name: 'Moufid',
+	location: 'Toronto'
+};
+
+var objTwo = {
+	age: 32,
+	...objOne
+};
+
+console.log(objTwo);
+
 ReactDOM.render(
 	<h1>Boilerplate app!</h1>,
 	document.getElementById('app')
