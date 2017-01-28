323172596221cce2c2d1ac9ca4ee58d9599120df
Moufid Larbi <moufid4@gmail.com>
2017-01-27 19:28:21 -0500

    added router

--- app/app.jsx
+++ app/app.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 ReactDOM.render(
 	<h1>ReactWeather</h1>,
