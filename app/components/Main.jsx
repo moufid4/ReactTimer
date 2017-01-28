96be8b6df0d821934bd8eb39dd5f37cd101183f8
Moufid Larbi <moufid4@gmail.com>
2017-01-28 12:56:16 -0500

    added nav component

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,9 +1,13 @@
 var React = require('react');
+var Nav = require('Nav');
 
 var Main = React.createClass({
 	render: function () {
 		return (
-			<h2>Main Components</h2>
+			<div>
+				<Nav/>
+				<h2>Main Components</h2>
+			</div>
 		);
 	}
 });
--- public/bundle.js
+++ public/bundle.js
@@ -26536,20 +26536,48 @@
 	'use strict';
 
 	var React = __webpack_require__(1);
+	var Nav = __webpack_require__(234);
 
 	var Main = React.createClass({
 		displayName: 'Main',
 
 		render: function render() {
 			return React.createElement(
-				'h2',
+				'div',
 				null,
-				'Main Components'
+				React.createElement(Nav, null),
+				React.createElement(
+					'h2',
+					null,
+					'Main Components'
+				)
 			);
 		}
 	});
 
 	module.exports = Main;
+
+/***/ },
+/* 234 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var React = __webpack_require__(1);
+
+	var Nav = React.createClass({
+		displayName: 'Nav',
+
+		render: function render() {
+			return React.createElement(
+				'h2',
+				null,
+				'Nav Components'
+			);
+		}
+	});
+
+	module.exports = Nav;
 
 /***/ }
 /******/ ]);
