02237ade129e112d7e99c15fe4eeaa57189e277d
Moufid Larbi <moufid4@gmail.com>
2017-01-27 19:27:10 -0500

    cleaned up code

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-console.log(objTwo);
-
 ReactDOM.render(
-	<h1>Boilerplate app!</h1>,
+	<h1>ReactWeather</h1>,
 	document.getElementById('app')
 );
