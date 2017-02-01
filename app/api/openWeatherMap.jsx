c11eb2b476741642b09f5dbbb0292200507feaea
Moufid Larbi <moufid4@gmail.com>
2017-02-01 13:50:16 -0500

    added openWeaterMap api

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,20 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=612c468acc18111a7f45ec36065ff570&units=metric';
+
+module.exports = {
+	getTemp: function (city) {
+		var encodedCity = encodeURIComponent(city);
+		var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedCity}`;
+		
+		return axios.get(requestUrl).then(function (res) {
+			if (res.data.cod && res.data.message) {
+				throw new Error(res.data.message);
+			} else {
+				return res.data.main.temp;
+			}
+		}, function (res) {
+			throw new Error(res.data.message);
+		});
+	}
+}
