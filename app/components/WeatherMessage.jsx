c868f0a92922d6513fd1ac92f58b9f19faa7f33f
Moufid Larbi <moufid4@gmail.com>
2017-02-01 19:57:30 -0500

    error fix

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,14 +1,14 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-	render: function() {
-		var {temp, city} = this.props;
+// var WeatherMessage = React.createClass({
+// 	render: function() {
+// 		var {temp, city} = this.props;
 		
-		return (
-			<h3>It is {temp} in {city}.</h3>
-		)
-	}
-});
+// 		return (
+// 			<h3>It is {temp} in {city}.</h3>
+// 		)
+// 	}
+// });
 
 var WeatherMessage = (props) => {
 	var {temp, city} = props;
