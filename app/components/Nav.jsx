0ef70f233563138b892a669bbdca75623cf73241
Moufid Larbi <moufid4@gmail.com>
2017-02-02 11:14:21 -0500

    added navbar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -16,11 +16,27 @@ var {Link, IndexLink} = require('react-router');
 
 var Nav = () => {
 	return (
-		<div>
-			<h2>Nav Components</h2>
-			<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-			<IndexLink to="about"  activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
-			<IndexLink to="examples"  activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
+		<div className="top-bar">
+			<div className="top-bar-left">
+				<ul className="menu">
+					<li className="menu-text">
+						React Weather
+					</li>
+					<li>
+						<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+					</li>
+					<li>
+						<IndexLink to="about"  activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
+					</li>
+					<li>
+						<IndexLink to="examples"  activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
+					</li>
+
+				</ul>
+			</div>
+			<div className="top-bar-right">
+
+			</div>
 		</div>
 	);
 }
