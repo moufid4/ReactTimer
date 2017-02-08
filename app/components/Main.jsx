d31ceda63b6ef244831edd43b86c878e115b482a
Moufid Larbi <moufid4@gmail.com>
2017-02-08 15:48:51 -0500

    styled countdown

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,9 +4,12 @@ var Navigation = require('Navigation');
 var Main = (props) => {
   return (
     <div>
-    	<Navigation/>
-      <p>Main.jsx rendered</p>
-      {props.children}
+	    <Navigation/>
+	    <div className="row">
+	    	<div className="column small-centered medium-6 large-4">
+			      {props.children}
+	    	</div>
+	    </div>
     </div>
   );
 }
